44c83fa8796016e375ab0cd49127902e03a76def
André <mrcouts@gmail.com>
2016-03-08 22:56:53 -0300

    Codigo separado em mais funções. Por algum motivo muito estranho, a simulação está dando um resultado um pouco diferente =p

--- Simulação e Modelagem/DynInvNum.py
+++ Simulação e Modelagem/DynInvNum.py
@@ -4,25 +4,47 @@ import numpy as np
 from InKinNum import *
 from DynamicsDeduction import *
 
-PARAMETROS = [(symbols('l_0'), par[l0]),
-              (symbols('l_1'), par[l1]),
-              (symbols('l_2'), par[l2]),
-              (symbols('lg_1'), 0.5*par[l1]),
-              (symbols('lg_2'), 0.5*par[l2]),
-              (symbols('m_1'), 0.143),
-              (symbols('m_2'), 0.171),
-              (symbols('Jz_1'), (0.143*par[l1]**2)/12.0),
-              (symbols('Jz_2'), (0.171*par[l2]**2)/12.0),
-              (symbols('g'), 9.8)         
-]
+PARAMETROS = {symbols('l_0'): par[l0],
+              symbols('l_1'): par[l1],
+              symbols('l_2'): par[l2],
+              symbols('lg_1'): 0.5*par[l1],
+              symbols('lg_2'): 0.5*par[l2],
+              symbols('m_1'): 0.143,
+              symbols('m_2'): 0.171,
+              symbols('Jz_1'): (0.143*par[l1]**2)/12.0,
+              symbols('Jz_2'): (0.171*par[l2]**2)/12.0,
+              symbols('g'): 9.8         
+}
                   
 rep = lambda vec1,vec2: [(vec1[i],vec2[i]) for i in xrange(len(vec1))]
+
+C_s = C_.subs(PARAMETROS)
+Z_s = Z_.subs(PARAMETROS)
+dC_s = dC_.subs(PARAMETROS)
+M_s = M_.subs(PARAMETROS)
+v_s = v_.subs(PARAMETROS)
+g_s = g_.subs(PARAMETROS)
+
+C_n = lambda q_n:       SMatrix( ( C_s.M_.subs(rep( list(q_.M_) ,q_n))).evalf(), C_.rowl_, C_.coll_)
+Z_n = lambda q_n:       SMatrix( ( Z_s.M_.subs(rep( list(q_.M_) ,q_n))).evalf(), Z_.rowl_, Z_.coll_) 
+M_n = lambda q_n:       SMatrix( ( M_s.M_.subs(rep( list(q_.M_) ,q_n))).evalf(), M_.rowl_, M_.coll_)
+g_n = lambda q_n:       SMatrix( ( g_s.M_.subs(rep( list(q_.M_) ,q_n))).evalf(), g_.rowl_, g_.coll_)
+dC_n= lambda q_n, p_n:  SMatrix( (dC_s.M_.subs(rep( list(p_.M_), p_n)).subs(rep( list(q_.M_) ,q_n))).evalf(), dC_.rowl_, dC_.coll_)
+v_n = lambda q_n, p_n:  SMatrix( ( v_s.M_.subs(rep( list(p_.M_), p_n)).subs(rep( list(q_.M_) ,q_n))).evalf(), v_.rowl_, v_.coll_)
+  
+def tau_n(qh_t, ph_t, dph_t):
+    q_t = fInvKin(qh_t)
+    C_t = C_n(q_t)
+    p_t = C_t*SMatrix( ph_t, ph_.rowl_)
+    dC_t = dC_n(q_t, p_t.M_)
+    dp_t = dC_t*SMatrix( ph_t ,ph_.rowl_) + C_t*SMatrix( dph_t,ph_.rowl_)
+    Z_t = Z_n(q_t)
+    M_t = M_n(q_t)
+    v_t = v_n(q_t, p_t.M_)
+    g_t = g_n(q_t)    
+    tau_t = Z_t.inv()*C_t.T()*( M_t*dp_t + v_t + g_t )
+    return tau_t
     
-C_n = lambda q_n: SMatrix( (C_.M_.subs(rep( list(q_.M_) ,q_n)).subs(PARAMETROS)).evalf(), C_.rowl_, C_.coll_)
-dC_n = lambda q_n, p_n: SMatrix( (dC_.M_.subs( rep(list(p_.M_), p_n) ).subs(rep( list(q_.M_) ,q_n)).subs(PARAMETROS)).evalf(), dC_.rowl_, dC_.coll_)  
-Z_n = lambda q_n: SMatrix( (Z_.M_.subs(rep( list(q_.M_) ,q_n)).subs(PARAMETROS)).evalf(), Z_.rowl_, Z_.coll_)  
-
-
 #Parametros da trajetoria
 r = 0.085
 x0 = 0.0
@@ -50,26 +72,7 @@ d2xt=[-(w**2)*r*cos(w*T) for T in t]
 d2yt=[-(w**2)*r*sin(w*T) for T in t]
 d2Xt=[Matrix([d2xt[i],d2yt[i]]) for i in xrange(nt)]
 
-q_t = []
-C_t = []
-dC_t = []
-Z_t = []
-p_t = []
-dp_t = []
-tau_t = []
-
-M_n = SMatrix( M_.M_.subs(PARAMETROS), M_.rowl_, M_.coll_)
-v_n = SMatrix( v_.M_.subs(PARAMETROS), v_.rowl_)
-g_n = SMatrix( g_.M_.subs(PARAMETROS), g_.rowl_)
-
-for i in xrange(nt):
-    q_t.append(fInvKin(Xt[i]))
-    C_t.append(C_n(q_t[i]))
-    Z_t.append(Z_n(q_t[i]))
-    p_t.append(C_t[i]*SMatrix( dXt[i],ph_.rowl_))
-    dC_t.append(dC_n(q_t[i], p_t[i].M_))
-    dp_t.append( dC_t[i]*SMatrix( dXt[i],ph_.rowl_) + C_t[i]*SMatrix( d2Xt[i],ph_.rowl_) )
-    tau_t.append( Z_t[i].inv()*C_t[i].T()*( M_n*dp_t[i] + v_n + g_n ) )
+tau_t = [tau_n(Xt[i], dXt[i], d2Xt[i]) for i in range(nt)]
 
 t_np = np.linspace(t[0], t[-1], len(t))
 tau1_np = t_np.copy()
