8a4d9731046fd9f4fea3539fe2c1265dbaf2b9b0
André <mrcouts@gmail.com>
2016-06-01 01:44:09 -0300

    Bolando o esquema de como representar se é junta de rotação ou prismática

--- Simulação e Modelagem/C++/hello.cpp
+++ Simulação e Modelagem/C++/hello.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include "SomeUtilities.h"
 #include "Serial.h"
-#include <stdio.h>  /* for printf */
 
 #define PI 3.14159265358979323846264338327950288
 
 mat fDH(vec q0_, vec l_, vec lg_){
     mat M;
-    M << l_(0) << 0 << 0 << q0_(0) << -l_(0) + lg_(0) << 0 << 0 << endr
-      << l_(1) << 0 << 0 << q0_(1) << -l_(1) + lg_(1) << 0 << 0 << endr;
+    M << l_(0) << 0 << 0 << q0_(0) << -l_(0) + lg_(0) << 0 << 0 << true << endr
+      << l_(1) << 0 << 0 << q0_(1) << -l_(1) + lg_(1) << 0 << 0 << true << endr;
     return M;}
 
 //Simple C++ program
@@ -17,6 +16,7 @@ int main(void)
     cout << "Hello! This is a C++ program." << endl;
     cout << H_d(1, 0.1, 2, 0.2) << endl;
 
+    //bool array[] = {true, true};
     Serial RR = Serial(2, {0.1, 0.1}, {0.05, 0.05}, &fDH);
     RR.Doit({PI/4, PI/4});
     //	RR.fDH = fDH;
@@ -29,6 +29,7 @@ int main(void)
     cout << RR.o__ << endl;
     cout << RR.og__ << endl;
     cout << RR.fDH(RR.l_, RR.l_, RR.lg_) << endl;
+
 
     return 0;
 }
