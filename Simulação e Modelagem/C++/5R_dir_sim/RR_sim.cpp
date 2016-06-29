a764a247d625a069277c8e073596449b456aae97
André <mrcouts@gmail.com>
2016-06-29 19:36:19 -0300

    Começando o programa de simulação do 5R

---
+++ Simulação e Modelagem/C++/5R_dir_sim/RR_sim.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "SomeUtilities.h"
+#include "Serial.h"
+#include "RR.h"
+#include "FLControlLaw.h"
+#include "Acceleration.h"
+#include "RK.h"
+
+vec r_(double t){
+    double w1 = 10;
+    double w2 = 15;
+    return {sin(w1*t),sin(w2*t)}; }
+
+vec dr_(double t){
+    double w1 = 10;
+    double w2 = 15;
+    return {w1*cos(w1*t),w2*cos(w2*t)}; }
+
+vec d2r_(double t){
+    double w1 = 10;
+    double w2 = 15;
+    return {-w1*w1*sin(w1*t), -w2*w2*sin(w2*t)}; }
+
+Dy* (dy_comp)(vec q0_, vec q1_){
+    Dy *dy;
+    dy = new Dy(2);
+    return dy; }
+
+class _5R_{
+public:
+    _5R_(){
+        M_.zeros(6,6);
+        v_.zeros(6);
+        g_.zeros(6);
+
+        cube I__; I__.zeros(3,3,2);
+        I__.slice(0) << 0 << 0        << 0        << endr
+                     << 0 << 171.6e-6 << 0        << endr
+                     << 0 << 0        << 171.6e-6 << endr;
+
+        I__.slice(1) << 0 << 0        << 0        << endr
+                     << 0 << 320.6e-6 << 0        << endr
+                     << 0 << 0        << 320.6e-6 << endr;
+
+        RR1 = new Serial(2, {0.12, 0.15}, {0.5*0.12, 0.5*0.15},{0.143, 0.171}, I__ , {0, -9.8,0}, &fDH_RR);
+        RR2 = new Serial(2, {0.12, 0.15}, {0.5*0.12, 0.5*0.15},{0.143, 0.171}, I__ , {0, -9.8,0}, &fDH_RR);
+        P = new Mecanismo(2); 
+    }
+    ~_5R_(){}
+    void Doit(vec q0_, vec q1_){
+        P.Doit(q0_(span(0,1)), q1_(span(0,1)));
+        RR1.Doit(q0_(span(2,3)), q1_(span(2,3)));
+        RR2.Doit(q0_(span(4,5)), q1_(span(4,5)));
+        v_ = join_cols(P->vh_, join_cols(RR1->vh_, RR2->vh_) );
+        g_ = join_cols(P->gh_, join_cols(RR1->gh_, RR2->gh_) );
+    }
+    mat M_;
+    vec v_;
+    vec g_;
+    Serial *RR1;
+    Serial *RR2;
+    Mecanismo *P;
+};
+
+int main(void){
+    int dof = 2;
+    cube I__; I__.zeros(3,3,dof);
+    I__.slice(0) << 0 << 0        << 0        << endr
+                 << 0 << 171.6e-6 << 0        << endr
+                 << 0 << 0        << 171.6e-6 << endr;
+
+    I__.slice(1) << 0 << 0        << 0        << endr
+                 << 0 << 320.6e-6 << 0        << endr
+                 << 0 << 0        << 320.6e-6 << endr;
+
+    Serial RR  = Serial(2, {0.12, 0.15}, {0.5*0.12, 0.5*0.15},{0.143, 0.171}, I__ , {0, -9.8,0}, &fDH_RR);
+
+    FLControlLaw FL = FLControlLaw(dof, 100.0, 20.0, &r_, &dr_, &d2r_, &RR);
+    Acceleration AC = Acceleration(dof, &RR, &FL);
+    //vec u; u.zeros(dof);
+    //u = FL.Doit(0, r_(0.1), dr_(0.1));
+    //cout << u << endl;
+    //vec v; v.zeros(2*dof);
+    //v = AC.f_(0, join_vert(r_(0.1), dr_(0.1)) );
+    //cout << v << endl;
+
+    RK rk = RK("RK8", &AC);
+    rk.Doit(0.001, 10.0, join_vert(r_(0.0), dr_(0.0)) );
+    for(uint i = 0; i< rk.t_.n_rows; i++)
+        cout << rk.t_(i) << "; " << rk.u__(0,0,i)  << "; " << rk.u__(1,0,i) << "; " << endl;
+        //cout << rk.t_(i) << "; " << r_(rk.t_(i))(1) - rk.y__(1,0,i)  << "; " << dr_(rk.t_(i))(1) - rk.y__(3,0,i) << "; " << endl;
+
+    return 0; }
---
+++ Simulação e Modelagem/C++/5R_dir_sim/RR_sim.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from math import pi as Pi
+from txt2py import *
+
+A = txt2py("RR_sim.txt")
+
+t_np = np.array([A[i][0] for i in range(len(A))])
+tau1_np = np.array([A[i][1] for i in range(len(A))])
+tau2_np = np.array([A[i][2] for i in range(len(A))])
+
+plt.figure()
+plt.plot(t_np, tau1_np, 'r')
+#plt.xscale('log')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('title')
+plt.show()
+
+plt.figure()
+plt.plot(t_np, tau2_np, 'r')
+#plt.xscale('log')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('title')
+plt.show()
