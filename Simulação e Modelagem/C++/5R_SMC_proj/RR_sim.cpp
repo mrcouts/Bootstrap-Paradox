fd03deb9a5e9a02dfc0f8a6a88e0423a99bcd142
André <mrcouts@gmail.com>
2016-08-19 20:11:47 -0300

    Começando o projeto do controlador por SMC

---
+++ Simulação e Modelagem/C++/5R_SMC_proj/RR_sim.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "SomeUtilities.h"
+#include "Serial.h"
+#include "RR.h"
+#include "FLControlLaw.h"
+#include "Acceleration.h"
+#include "RK.h"
+#include "Parallel.h"
+#include "GNR2.h"
+
+int main(){
+
+    Mecanismo P = Mecanismo(2);
+
+    double lg1 = 0.5*0.12;
+    double lg2 = 0.5*0.15;
+    double m1 = 0.143;
+    double m2 = 0.171;
+    double Jz1 = 171.6e-6;
+    double Jz2 = 320.6e-6;
+    double sigma = 0.1;
+
+    cube I__; I__.zeros(3,3,2);
+    I__.slice(0) << 0 << 0   << 0   << endr
+                 << 0 << Jz1 << 0   << endr
+                 << 0 << 0   << Jz1 << endr;
+
+    I__.slice(1) << 0 << 0   << 0   << endr
+                 << 0 << Jz2 << 0   << endr
+                 << 0 << 0   << Jz2 << endr;
+
+    Serial RR1 = Serial(2, {0.12, 0.15}, {lg1, lg2},{m1, m2}, I__ , {0, -9.8,0}, &fDH_RR);
+    Serial RR2 = Serial(2, {0.12, 0.15}, {lg1, lg2},{m1, m2}, I__ , {0, -9.8,0}, &fDH_RR);
+    Serial **RR_ = new Serial* [2];
+    RR_[0] = &RR1;
+    RR_[1] = &RR2;
+
+    //Matrizes que descrevem a arquitetura do mecanismo
+    mat D_ = join_vert((mat)eye(2,2),2);
+    mat E_ = join_diag( Roty(0)(span(0,1),span(0,2)), Roty(PI)(span(0,1),span(0,2)) );
+    mat F_ = zeros(4,4);
+    vec f_ = {0.05,0,-0.05,0};
+
+    Parallel Robot = Parallel(2, &P, RR_, 2, {2,4}, D_, E_, F_, f_);
+    Reference RefObj = Reference(0.12, {0.08, 0.16}, {-0.08, 0.4});
+
+    uint nPI = 6;
+    uint nCases = (uint)pow(2,nPI);
+    //cube lg__; lg__.zeros(2, 1, nCases);
+    //cube m__; m__.zeros(2, 1, nCases);
+    //cube Jz__; Jz__.zeros(2, 1, nCases);
+
+    Mecanismo **P_ = new Mecanismo* [nCases];
+    Serial **RR1_ = new Serial* [nCases];
+    Serial **RR2_ = new Serial* [nCases];
+    Serial ***RR__ = new Serial** [nCases];
+    Parallel **Robot_ = new Parallel* [nCases];
+
+    uint cont = 0;
+    for(int i1=-1; i1<2; i1 = i1 + 2){
+        for(int i2=-1; i2<2; i2 = i2 + 2){
+            for(int i3=-1; i3<2; i3 = i3 + 2){
+                for(int i4=-1; i4<2; i4 = i4 + 2){
+                    for(int i5=-1; i5<2; i5 = i5 + 2){
+                        for(int i6=-1; i6<2; i6 = i6 + 2){
+                            I__(1,1,0) = Jz1*(1+sigma*i5);
+                            I__(2,2,0) = Jz1*(1+sigma*i5);
+                            I__(1,1,1) = Jz2*(1+sigma*i6);
+                            I__(2,2,1) = Jz2*(1+sigma*i6);
+                            P_[cont] = new Mecanismo(2);
+                            RR1_[cont] = new Serial(2, {0.12, 0.15}, {lg1*(1+sigma*i1), lg2*(1+sigma*i2)}, {m1*(1+sigma*i3), m2*(1+sigma*i4)}, I__ , {0, -9.8,0}, &fDH_RR);
+                            RR2_[cont] = new Serial(2, {0.12, 0.15}, {lg1*(1+sigma*i1), lg2*(1+sigma*i2)}, {m1*(1+sigma*i3), m2*(1+sigma*i4)}, I__ , {0, -9.8,0}, &fDH_RR);
+                            RR__[cont] = new Serial* [2];
+                            RR__[cont][0] = RR1_[cont];
+                            RR__[cont][1] = RR2_[cont];
+                            Robot_[cont] = new Parallel(2, P_[cont], RR__[cont], 2, {2,4}, D_, E_, F_, f_);
+                            cont++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //Plotar área de trabalho
+    double lx = 0.23;
+    double ly = 0.27;
+    double dl = 0.01;
+    uint nx = (lx/dl);
+    uint ny = (ly/dl);
+    Mat<int> M; M.zeros(ny,nx);
+    //mat M; M.zeros(ny,nx);
+    field<mat> fMh_(ny,nx);
+    for(uint i=0; i<ny; i++)
+        for(uint j=0; j<nx; j++)
+            fMh_(i,j).zeros(2,2);
+    double r = 0.07;
+    double x0 = 0.0;
+    double y0 = 0.17;
+
+    uint rows = ny;
+    uint cols = nx;
+
+    vec q0_ = {0.823167, 1.81774, 0.823167, 1.81774};
+
+    GNR2 gnr2 = GNR2("RK6", &Robot, 1e-6, 30);
+    //gnr2.Doit(q0_, {0.05,0.08});
+    //cout << gnr2.convergiu << endl;
+    //cout << gnr2.x_ << endl;
+    //cout << gnr2.res_ << endl;
+    //cout << gnr2.n << endl;
+
+    double x;
+    double y;
+    mat A2_;
+    mat Delta_; Delta_.zeros(2,2);
+    mat DeltaA_; DeltaA_.zeros(2,2);
+    vec delta_; delta_.zeros(2);
+    vec deltaA_; deltaA_.zeros(2);
+    for(uint i=0; i<rows; i++){
+        for(uint j=0; j<cols; j++){
+            x = j*dl+0.5*dl;
+            y = i*dl+0.5*dl;
+            gnr2.Doit(q0_, {x, y});
+            if(gnr2.convergiu){
+                A2_ = join_horiz(Robot.Ah_, join_horiz(Robot.Ao_.col(1), Robot.Ao_.col(3)) );
+                if(abs(det(Robot.Ao_)) < 1.6*1e-4 || abs(det(A2_)) < 1e-3 ) M(i,j) = 2;
+                else{
+                    M(i,j) = 1;
+                    Robot.Doit(Robot.q0_, Robot.q1_);
+                    fMh_(i,j) = Robot.dy->Mh_;
+                    for (uint k = 0; k < nCases; k++){
+                        Robot_[k]->Doit(Robot.q0_, Robot.q1_);
+                        DeltaA_ = solve(Robot_[k]->dy->Mh_, Robot.dy->Mh_) - eye(2,2);
+                        deltaA_ = solve(Robot_[k]->dy->Mh_, Robot.dy->gh_ - Robot_[k]->dy->gh_);
+                        Delta_ = arma::max(Delta_, abs(DeltaA_));
+                        delta_ = arma::max(delta_, abs(deltaA_));
+                    }
+                }
+                //M(i,j) = max(log(1 + 1.0/abs(det(Robot.Ao_))), log(1 + 1.0/abs(det(A2_))));
+            }
+            gnr2.convergiu = false;
+            if( ((x - x0)*(x - x0) + (y - y0)*(y - y0) <= r*r) && (M(i,j) != 2) )
+                M(i,j) = 3;
+        }
+    }
+
+    //for(uint i=0; i<rows; i++){
+    //    for(uint j=0; j<cols; j++){
+    //        cout << M(i,j) << ";" ;
+    //        if(j==cols-1) cout << endl;
+    //    }
+    //}
+
+    cout << Delta_ << endl;
+    cout << delta_ << endl;
+
+
+    return 0;
+}
