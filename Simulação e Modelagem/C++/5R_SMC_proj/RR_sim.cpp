af96a6f5c8c9b56ed6921b5f428f0c8570aa25da
André <mrcouts@gmail.com>
2016-08-19 23:12:07 -0300

    Agora calcula os deltas do a1, a2 e a12

--- Simulação e Modelagem/C++/5R_SMC_proj/RR_sim.cpp
+++ Simulação e Modelagem/C++/5R_SMC_proj/RR_sim.cpp
@@ -11,16 +11,19 @@
 #include "GNR2.h"
 
 int main(){
-
-    Mecanismo P = Mecanismo(2);
-
     double lg1 = 0.5*0.12;
     double lg2 = 0.5*0.15;
     double m1 = 0.143;
     double m2 = 0.171;
     double Jz1 = 171.6e-6;
     double Jz2 = 320.6e-6;
-    double sigma = 0.1;
+
+    double sigma1 = 0.1;
+    double sigma2 = 0.1;
+    double sigma3 = 0.1;
+    double sigma4 = 0.1;
+    double sigma5 = 0.3;
+    double sigma6 = 0.3;
 
     cube I__; I__.zeros(3,3,2);
     I__.slice(0) << 0 << 0   << 0   << endr
@@ -31,6 +34,7 @@ int main(){
                  << 0 << Jz2 << 0   << endr
                  << 0 << 0   << Jz2 << endr;
 
+    Mecanismo P = Mecanismo(2);
     Serial RR1 = Serial(2, {0.12, 0.15}, {lg1, lg2},{m1, m2}, I__ , {0, -9.8,0}, &fDH_RR);
     Serial RR2 = Serial(2, {0.12, 0.15}, {lg1, lg2},{m1, m2}, I__ , {0, -9.8,0}, &fDH_RR);
     Serial **RR_ = new Serial* [2];
@@ -48,9 +52,6 @@ int main(){
 
     uint nPI = 6;
     uint nCases = (uint)pow(2,nPI);
-    //cube lg__; lg__.zeros(2, 1, nCases);
-    //cube m__; m__.zeros(2, 1, nCases);
-    //cube Jz__; Jz__.zeros(2, 1, nCases);
 
     Mecanismo **P_ = new Mecanismo* [nCases];
     Serial **RR1_ = new Serial* [nCases];
@@ -65,13 +66,13 @@ int main(){
                 for(int i4=-1; i4<2; i4 = i4 + 2){
                     for(int i5=-1; i5<2; i5 = i5 + 2){
                         for(int i6=-1; i6<2; i6 = i6 + 2){
-                            I__(1,1,0) = Jz1*(1+sigma*i5);
-                            I__(2,2,0) = Jz1*(1+sigma*i5);
-                            I__(1,1,1) = Jz2*(1+sigma*i6);
-                            I__(2,2,1) = Jz2*(1+sigma*i6);
+                            I__(1,1,0) = Jz1*(1+sigma5*i5);
+                            I__(2,2,0) = Jz1*(1+sigma5*i5);
+                            I__(1,1,1) = Jz2*(1+sigma6*i6);
+                            I__(2,2,1) = Jz2*(1+sigma6*i6);
                             P_[cont] = new Mecanismo(2);
-                            RR1_[cont] = new Serial(2, {0.12, 0.15}, {lg1*(1+sigma*i1), lg2*(1+sigma*i2)}, {m1*(1+sigma*i3), m2*(1+sigma*i4)}, I__ , {0, -9.8,0}, &fDH_RR);
-                            RR2_[cont] = new Serial(2, {0.12, 0.15}, {lg1*(1+sigma*i1), lg2*(1+sigma*i2)}, {m1*(1+sigma*i3), m2*(1+sigma*i4)}, I__ , {0, -9.8,0}, &fDH_RR);
+                            RR1_[cont] = new Serial(2, {0.12, 0.15}, {lg1*(1+sigma1*i1), lg2*(1+sigma2*i2)}, {m1*(1+sigma3*i3), m2*(1+sigma4*i4)}, I__ , {0, -9.8,0}, &fDH_RR);
+                            RR2_[cont] = new Serial(2, {0.12, 0.15}, {lg1*(1+sigma1*i1), lg2*(1+sigma2*i2)}, {m1*(1+sigma3*i3), m2*(1+sigma4*i4)}, I__ , {0, -9.8,0}, &fDH_RR);
                             RR__[cont] = new Serial* [2];
                             RR__[cont][0] = RR1_[cont];
                             RR__[cont][1] = RR2_[cont];
@@ -87,15 +88,25 @@ int main(){
     //Plotar área de trabalho
     double lx = 0.23;
     double ly = 0.27;
-    double dl = 0.01;
+    double dl = 0.001;
     uint nx = (lx/dl);
     uint ny = (ly/dl);
     Mat<int> M; M.zeros(ny,nx);
     //mat M; M.zeros(ny,nx);
     field<mat> fMh_(ny,nx);
-    for(uint i=0; i<ny; i++)
-        for(uint j=0; j<nx; j++)
+    field<vec> fgh_(ny,nx);
+    field<vec> fa1_(ny,nx);
+    field<vec> fa2_(ny,nx);
+    field<vec> fa12_(ny,nx);
+    for(uint i=0; i<ny; i++){
+        for(uint j=0; j<nx; j++){
             fMh_(i,j).zeros(2,2);
+            fgh_(i,j).zeros(2);
+            fa1_(i,j).zeros(2);
+            fa2_(i,j).zeros(2);
+            fa12_(i,j).zeros(2);
+        }
+    }
     double r = 0.07;
     double x0 = 0.0;
     double y0 = 0.17;
@@ -106,19 +117,21 @@ int main(){
     vec q0_ = {0.823167, 1.81774, 0.823167, 1.81774};
 
     GNR2 gnr2 = GNR2("RK6", &Robot, 1e-6, 30);
-    //gnr2.Doit(q0_, {0.05,0.08});
-    //cout << gnr2.convergiu << endl;
-    //cout << gnr2.x_ << endl;
-    //cout << gnr2.res_ << endl;
-    //cout << gnr2.n << endl;
 
     double x;
     double y;
     mat A2_;
     mat Delta_; Delta_.zeros(2,2);
-    mat DeltaA_; DeltaA_.zeros(2,2);
     vec delta_; delta_.zeros(2);
-    vec deltaA_; deltaA_.zeros(2);
+    vec delta1_; delta1_.zeros(2);
+    vec delta2_; delta2_.zeros(2);
+    vec delta12_; delta12_.zeros(2);
+    vec v1_ = {1,0};
+    vec v2_ = {0,1};
+    vec v12_ = {1,1};
+    vec a1_;
+    vec a2_;
+    vec a12_;
     for(uint i=0; i<rows; i++){
         for(uint j=0; j<cols; j++){
             x = j*dl+0.5*dl;
@@ -129,14 +142,26 @@ int main(){
                 if(abs(det(Robot.Ao_)) < 1.6*1e-4 || abs(det(A2_)) < 1e-3 ) M(i,j) = 2;
                 else{
                     M(i,j) = 1;
-                    Robot.Doit(Robot.q0_, Robot.q1_);
+                    Robot.Doit(Robot.q0_, Robot.C_*v1_);
                     fMh_(i,j) = Robot.dy->Mh_;
+                    fgh_(i,j) = Robot.dy->gh_;
+                    fa1_(i,j) = Robot.dy->vh_;
+                    Robot.Doit(Robot.q0_, Robot.C_*v2_);
+                    fa2_(i,j) = Robot.dy->vh_;
+                    Robot.Doit(Robot.q0_, Robot.C_*v12_);
+                    fa12_(i,j) = Robot.dy->vh_ - fa1_(i,j) - fa2_(i,j);
                     for (uint k = 0; k < nCases; k++){
-                        Robot_[k]->Doit(Robot.q0_, Robot.q1_);
-                        DeltaA_ = solve(Robot_[k]->dy->Mh_, Robot.dy->Mh_) - eye(2,2);
-                        deltaA_ = solve(Robot_[k]->dy->Mh_, Robot.dy->gh_ - Robot_[k]->dy->gh_);
-                        Delta_ = arma::max(Delta_, abs(DeltaA_));
-                        delta_ = arma::max(delta_, abs(deltaA_));
+                        Robot_[k]->Doit(Robot.q0_, Robot.C_*v1_);
+                        Delta_ = arma::max(Delta_, abs(solve(Robot_[k]->dy->Mh_, fMh_(i,j)) - eye(2,2)));
+                        delta_ = arma::max(delta_, abs(solve(Robot_[k]->dy->Mh_, fgh_(i,j) - Robot_[k]->dy->gh_)));
+                        a1_ = Robot_[k]->dy->vh_;
+                        delta1_ = arma::max(delta1_, abs(solve(Robot_[k]->dy->Mh_, fa1_(i,j) - a1_)));
+                        Robot_[k]->Doit(Robot.q0_, Robot.C_*v2_);
+                        a2_ = Robot_[k]->dy->vh_;
+                        delta2_ = arma::max(delta2_, abs(solve(Robot_[k]->dy->Mh_, fa2_(i,j) - a2_)));
+                        Robot_[k]->Doit(Robot.q0_, Robot.C_*v12_);
+                        a12_ = Robot_[k]->dy->vh_ - a1_ - a2_;
+                        delta12_ = arma::max(delta12_, abs(solve(Robot_[k]->dy->Mh_, fa12_(i,j) - a12_)));
                     }
                 }
                 //M(i,j) = max(log(1 + 1.0/abs(det(Robot.Ao_))), log(1 + 1.0/abs(det(A2_))));
@@ -156,6 +181,9 @@ int main(){
 
     cout << Delta_ << endl;
     cout << delta_ << endl;
+    cout << delta1_ << endl;
+    cout << delta2_ << endl;
+    cout << delta12_ << endl;
 
 
     return 0;
