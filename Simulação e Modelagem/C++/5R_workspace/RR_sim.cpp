b84dd4d0a6e42bc97033827a6fc2fe453d00cb3f
Andre <mrcouts@gmail.com>
2018-01-08 21:08:56 -0200

    Demorei pra subir, mas vamos lá. Atualizado o esquema de obter a área de trabalho

--- Simulação e Modelagem/C++/5R_workspace/RR_sim.cpp
+++ Simulação e Modelagem/C++/5R_workspace/RR_sim.cpp
@@ -14,43 +14,79 @@ int main(){
 
     Mecanismo P = Mecanismo(2);
 
-        cube I__; I__.zeros(3,3,2);
-    I__.slice(0) << 0 << 0        << 0        << endr
-                 << 0 << 171.6e-6 << 0        << endr
-                 << 0 << 0        << 171.6e-6 << endr;
+    cube I1__; I1__.zeros(3,3,2);
+    I1__.slice(0) << 0 << 0                       << 0          << endr
+                  << 0 << 107.307e-6 + 146.869e-6 << 0          << endr
+                  << 0 << 0                       << 107.307e-6 + 146.869e-6 << endr;
 
-    I__.slice(1) << 0 << 0        << 0        << endr
-                 << 0 << 320.6e-6 << 0        << endr
-                 << 0 << 0        << 320.6e-6 << endr;
+    I1__.slice(1) << 0 << 0        << 0        << endr
+                  << 0 << 438.0e-6 << 0        << endr
+                  << 0 << 0        << 438.0e-6 << endr;
 
-    Serial RR1 = Serial(2, {0.12, 0.15}, {0.5*0.12, 0.5*0.15},{0.143, 0.171}, I__ , {0, -9.8,0}, &fDH_RR);
-    Serial RR2 = Serial(2, {0.12, 0.15}, {0.5*0.12, 0.5*0.15},{0.143, 0.171}, I__ , {0, -9.8,0}, &fDH_RR);
+    cube I2__; I2__.zeros(3,3,2);
+    I2__.slice(0) << 0 << 0                        << 0          << endr
+                  << 0 << 107.307e-6 + 188.738e-6  << 0          << endr
+                  << 0 << 0                        << 107.307e-6 + 188.738e-6  << endr;
+
+    I2__.slice(1) << 0 << 0          << 0          << endr
+                  << 0 << 301.679e-6 << 0          << endr
+                  << 0 << 0          << 301.679e-6 << endr;
+
+    Serial RR1 = Serial(2, {0.12, 0.16}, {0.06, 0.078},{0.062, 0.124}, I1__ , {0, 0, 9.8}, &fDH_RR);
+    Serial RR2 = Serial(2, {0.12, 0.16}, {0.06, 0.058},{0.062, 0.097}, I2__ , {0, 0, 9.8}, &fDH_RR);
     Serial **RR_ = new Serial* [2];
     RR_[0] = &RR1;
     RR_[1] = &RR2;
 
     //Matrizes que descrevem a arquitetura do mecanismo
+    double l0 = 0.05;
     mat D_ = join_vert((mat)eye(2,2),2);
     mat E_ = join_diag( Roty(0)(span(0,1),span(0,2)), Roty(PI)(span(0,1),span(0,2)) );
     mat F_ = zeros(4,4);
-    vec f_ = {0.05,0,-0.05,0};
+    vec f_ = {l0,0,-l0,0};
 
     Parallel Robot = Parallel(2, &P, RR_, 2, {2,4}, D_, E_, F_, f_);
     Reference RefObj = Reference(0.12, {0.08, 0.16}, {-0.08, 0.4});
 
     //Plotar área de trabalho
-    double lx = 0.23;
-    double ly = 0.27;
-    double dl = 0.00025;
-    uint nx = (lx/dl);
-    uint ny = (ly/dl);
-    Mat<int> M; M.zeros(ny,nx);
+    uint nx = 96.0;
+    uint ny = 56.0;
+    double lx = 0.24;
+    double ly = 0.28;
+    double xi = -lx;
+    double xf = lx;
+    double yi = 0.0;
+    double yf = ly;
+    double dx = (xf-xi)/(nx-1);
+    double dy = (yf-yi)/(ny-1);
+    double dl = 0.5*(dx+dy);
+    
+    Mat<int> M; M.zeros(nx,ny);
+    field<mat> fZ_(nx,ny);
+    field<mat> fMh_(nx,ny);
+    field<vec> fgh_(nx,ny);
+    field<vec> fa1_(nx,ny);
+    field<vec> fa2_(nx,ny);
+    field<vec> fa12_(nx,ny);
+    for(uint i=0; i<nx; i++){
+        for(uint j=0; j<ny; j++){
+            fZ_(i,j).zeros(2,2);
+            fMh_(i,j).zeros(2,2);
+            fgh_(i,j).zeros(2);
+            fa1_(i,j).zeros(2);
+            fa2_(i,j).zeros(2);
+            fa12_(i,j).zeros(2);
+        }
+    }
+    vec v1_ = {1,0};
+    vec v2_ = {0,1};
+    vec v12_ = {1,1};
     double r = 0.07;
     double x0 = 0.0;
     double y0 = 0.17;
 
-    uint rows = ny;
-    uint cols = nx;
+    uint rows = nx;
+    uint cols = ny;
 
     vec q0_ = {0.823167, 1.81774, 0.823167, 1.81774};
 
@@ -66,14 +102,25 @@ int main(){
     mat A2_;
     for(uint i=0; i<rows; i++){
         for(uint j=0; j<cols; j++){
-            x = j*dl+0.5*dl;
-            y = i*dl+0.5*dl;
+            x = xi + i*dx;
+            y = yi + j*dy;
             gnr2.Doit(q0_, {x, y});
             if(gnr2.convergiu){
                 q0_ = gnr2.x_;
                 A2_ = join_horiz(Robot.Ah_, join_horiz(Robot.Ao_.col(1), Robot.Ao_.col(3)) );
-                if(abs(det(Robot.Ao_)) < 1.6*1e-4 || abs(det(A2_)) < 1e-10 ) M(i,j) = 2;
-                else M(i,j) = 1;
+                if(abs(det(Robot.Ao_)) < 1.6*1e-6 || abs(det(A2_)) < 1e-11 ) M(i,j) = 2;
+                else{ 
+                	M(i,j) = 1;
+                	Robot.Doit(Robot.q0_, join_vert(v1_, -solve(Robot.Ao_, Robot.Ah_*v1_)) );
+                	fZ_(i,j) = Robot.Z_;
+                    fMh_(i,j) = Robot.dy->Mh_;
+                    fgh_(i,j) = Robot.dy->gh_;
+                    fa1_(i,j) = Robot.dy->vh_;
+                    Robot.Doit(Robot.q0_, Robot.C_*v2_);
+                    fa2_(i,j) = Robot.dy->vh_;
+                    Robot.Doit(Robot.q0_, Robot.C_*v12_);
+                    fa12_(i,j) = Robot.dy->vh_ - fa1_(i,j) - fa2_(i,j);
+                }
             }
             gnr2.convergiu = false;
             if( ((x - x0)*(x - x0) + (y - y0)*(y - y0) <= (r+dl)*(r+dl) ) && ((x - x0)*(x - x0) + (y - y0)*(y - y0) >= (r-dl)*(r-dl) ) && (M(i,j) != 2) )
@@ -87,6 +134,14 @@ int main(){
             if(j==cols-1) cout << endl;
         }
     }
+
+
+    fZ_.save("fZ_field");
+    fMh_.save("fMh_field");
+    fgh_.save("fgh_field");
+    fa1_.save("fa1_field");
+    fa2_.save("fa2_field");
+    fa12_.save("fa12_field");
 
 
     return 0;
--- Simulação e Modelagem/C++/hello/hello.cpp
+++ Simulação e Modelagem/C++/hello/hello.cpp
@@ -1,18 +1,161 @@
 #include <iostream>
 #include <armadillo>
+#define PI 3.1415926535897932384626433832795028841971693993751
 
 using namespace std;
 using namespace arma;
 
+double interpolacao(double x, double x0, double xf, vec v)
+{
+    int N = v.n_rows;
+    if(x < x0) return v(0);
+    else if(x > xf) return v(N-1);
+    else{
+        double dx = (xf-x0)/(N-1);
+        double index = (x - x0)/dx;
+        int index_esq = floor(index);
+        int index_dir = ceil(index);
+        double y0 = v(index_esq);
+        double yf = v(index_dir);
+        double y = y0 + (yf - y0)*(index - index_esq);
+        return y;   
+    }
+}
+
+double interpolacao_2D(double x, double y,  double x0, double y0, double xf, double yf, mat M)
+{
+    if( x >= x0 && x <= xf && yf >= y0 && y <= yf ){
+        int Nx = M.n_rows;
+        int Ny = M.n_cols;
+        double dx = (xf-x0)/(Nx-1);
+        double dy = (yf-y0)/(Ny-1);
+        double index_x = (x - x0)/dx;
+        int index_x1 = floor(index_x);
+        int index_x2 = ceil(index_x);
+        double index_y = (y - y0)/dy;
+        int index_y1 = floor(index_y);
+        int index_y2 = ceil(index_y);
+        double z11 = M(index_x1, index_y1);
+        double z12 = M(index_x1, index_y2);
+        double z21 = M(index_x2, index_y1);
+        double z22 = M(index_x2, index_y2);
+        double z1 = z11 + (z21 - z11)*(index_x - index_x1);
+        double z2 = z12 + (z22 - z12)*(index_x - index_x1);
+        double z = z1 + (z2 - z1)*(index_y - index_y1);
+        return z;
+    }
+    else return 0; 
+}
+
+mat interpolacao_2D_mat(double x, double y,  double x0, double y0, double xf, double yf, field<mat> M)
+{
+    if( x >= x0 && x <= xf && yf >= y0 && y <= yf ){
+        int Nx = M.n_rows;
+        int Ny = M.n_cols;
+        double dx = (xf-x0)/(Nx-1);
+        double dy = (yf-y0)/(Ny-1);
+        double index_x = (x - x0)/dx;
+        int index_x1 = floor(index_x);
+        int index_x2 = ceil(index_x);
+        double index_y = (y - y0)/dy;
+        int index_y1 = floor(index_y);
+        int index_y2 = ceil(index_y);
+        mat z11 = M(index_x1, index_y1);
+        mat z12 = M(index_x1, index_y2);
+        mat z21 = M(index_x2, index_y1);
+        mat z22 = M(index_x2, index_y2);
+        mat z1 = z11 + (z21 - z11)*(index_x - index_x1);
+        mat z2 = z12 + (z22 - z12)*(index_x - index_x1);
+        mat z = z1 + (z2 - z1)*(index_y - index_y1);
+        return z;
+    }
+    else return 0;
+}
+
 int main(void){
-	cout << "Hello! This is a C++ program." << endl;
+	//cout << "Hello! This is a C++ program." << endl;
     mat A; A.zeros(2,2);
     A << 1 << 2 << endr
       << 3 << 4 << endr;
     vec b = {1, 2};
-    vec x = solve(A,b);
-    A.print("A = ");
+    //vec x = solve(A,b);
+    /*A.print("A = ");
     b.print("b = ");
     x.print("x = ");
+    cout << endl;*/
+
+    /*
+    int n = 20;
+    vec v(n, fill::zeros);
+    for(int i = 0; i<n; i++) v(i) = sin(2*PI*i/(n-1));
+    cout << v << endl;
+
+    double x0 = 0;
+    double xf = 2*PI;
+    double x = 3*2*PI/(n-1)+0.01;
+
+    cout << interpolacao(x, x0, xf, v) << endl;
+    cout << sin(x) << endl;
+    */
+
+    /*
+    int n = 400;
+    int nx = n;
+    int ny = n;
+    double x0 = -5.0;
+    double y0 = -6.0;
+    double xf = 4;
+    double yf = 3;
+    double dx = (xf-x0)/(nx-1);
+    double dy = (yf-y0)/(ny-1);
+    double x, y;
+
+    field<mat> M(nx, ny);
+    for(int i = 0; i<nx; i++){
+        x = x0 + i*dx;
+        for(int j = 0; j<ny; j++){
+            y = y0 + j*dy;
+            M(i,j) = zeros(2,2);
+            M(i,j)(0,0) = sin( x*y ) ;
+            M(i,j)(0,1) = cos( x*y ) ;
+            M(i,j)(1,0) = sin( x*x ) ;
+            M(i,j)(1,1) = cos( y*y ) ;
+        }
+    }
+    //cout << M << endl;
+    //M.save("mat_field");
+
+
+
+    double xis = -1;
+    double ypi = -2;
+    cout << interpolacao_2D_mat(xis,ypi, x0, y0, xf, yf, M) << endl;
+    //cout << interpolacao(xis, x0, xf, M.col(0)) << endl;
+    //cout << sin(xis*ypi) << endl;
+    */
+
+    //Plotar área de trabalho
+    uint nx = 96.0;
+    uint ny = 56.0;
+    double lx = 0.24;
+    double ly = 0.28;
+    double xi = -lx;
+    double xf = lx;
+    double yi = 0.0;
+    double yf = ly;
+    field<mat> fZ_(ny,nx); fZ_.load("fZ_field");
+    field<mat> fMh_(ny,nx); fMh_.load("fMh_field");
+    //field<vec> fgh_(ny,nx); fgh_.load("fgh_field");
+    field<vec> fa1_(ny,nx); fa1_.load("fa1_field");
+    field<vec> fa2_(ny,nx); fa2_.load("fa2_field");
+    field<vec> fa12_(ny,nx); fa12_.load("fa12_field");
+
+    fMh_(20,40).print("Mh_ = ");
+    fZ_(20,40).t().print("Z^T = ");
+    //cout << fMh_.n_rows << endl;
+    //cout << fMh_.n_cols << endl;
+    cout << endl << "Mh_ = " << endl <<interpolacao_2D_mat(-0.05, 0.20, xi, yi, xf, yf, fMh_) << endl;
+
+
     return 0;
 }
